9df04f216d8b0b7f65cb36043b5961b8c3968b00
Bhaveshpchavda <108624512+Bhaveshpchavda@users.noreply.github.com>
2023-02-05 22:19:09 +0530

    commit

---
+++ FET/PR7/validate.js
@@ -0,0 +1,7 @@
+export function validate_info(name,pass){
+    if(name=='Bhavesh' && pass=='1234'){
+      return 'Valid';
+    }
+    return 0;
+  }
+  
---
+++ PYTHON/classW.py
@@ -0,0 +1,5 @@
+# with open('sample.txt','w') as f:
+#     data = f.write("Hello world ")
+ 
+open('sample.txt','r')
+font = f.read()
