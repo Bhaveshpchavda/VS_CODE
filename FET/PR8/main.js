3708a48854fe7ef8688bf5f359a7e5f574604003
Bhaveshpchavda <108624512+Bhaveshpchavda@users.noreply.github.com>
2023-02-09 23:50:15 +0530

    lab8 updated

---
+++ FET/PR8/main.js
@@ -0,0 +1,20 @@
+let kayoPromise = new Promise(function(Resolve,Reject) {
+     let health = prompt("Are you healthy or not? [Y/N]")
+     
+     if (health == 'y') {
+     let numOfCakes = prompt("How many cakes can you bake?");
+     Resolve("I have " + numOfCakes + " cakes and I'm happy!! : )");
+     } 
+     else if(health == 'n') {
+     Reject("There is no cake and I'm sad! : (");
+     }
+     else {
+     alert("Invalid Input, Try Again!");
+     location.reload();
+    }
+    });
+    kayoPromise.then(result => {alert(result);})
+    .catch(error => {alert(error);})
+    .finally(() => {alert('I still have party');})
+   
+    
