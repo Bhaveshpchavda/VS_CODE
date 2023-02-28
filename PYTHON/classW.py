001ae186f0eb3faff98594c4ecf7c491f08c0b32
Bhaveshpchavda <108624512+Bhaveshpchavda@users.noreply.github.com>
2023-02-28 23:22:38 +0530

    Practice file

--- PYTHON/classW.py
+++ PYTHON/classW.py
@@ -53,6 +53,77 @@
 # d= a.find('h')
 # print(d)
 
-x='hell'
-print(x)
-print(type(x))
+# x='hell'
+# print(x)
+# print(type(x))
+# n=123.82341 
+# print(n)   
+# type(n) 
+
+# List
+# list = ['bhavesh','vimal','akshit',101,123.12]
+# print(type(list))
+# temp = list.pop()
+# print(temp)
+# print(list)
+# list.remove('bhavesh')
+# list.clear()
+# p=list.count('vimal')
+# print(p)
+#  list = ['ICT', 'Guni', 'Ahmedabad', 'ICT', 'Gujarat']
+# list.reverse()
+# print(list)
+# y=list.sort()
+# y=list
+# print(list)
+# y=list.sort(reverse=True)
+# print(list)
+# list = [1,2,3,4,5,4,5,6]
+# print(list)
+# list[6]='bhavesh'
+# print(list)
+# mylist=[9,8,7,list]
+# print(mylist)
+# print(len(list))
+# print(len(mylist))
+# print(mylist[2])
+
+# Tupple
+# tuple=(1,2,3,4,5,6)
+# print(type(tupple))
+# print(tuple[5])
+# r=tuple[2:-2]
+# print(r)
+# print(tuple+tuple)
+# print(tuple[-3])
+# my_tuple=(12,23,34,tuple)
+# print(my_tuple)
+# print(len(my_tuple))
+# print(my_tuple[3])
+# t3 = (1567, 2876, -0.34, 'apple', (1,2,3), [4,5,6])
+# print(t3)
+# print(t3[-1][-3])
+
+# Dictionary 
+# d1 = {'brand':"Ford", 'modal':"Mustang", 'year':1970}
+# print(type(d1))
+# print(d1['modal'])
+# d1['engine']='720HP'
+# d1['Yugam'] = 10023
+# # print(d1)
+# print(d1.items())
+# print(d1.keys())
+# print(d1.values())
+# brands = ['ford', 'honda', 'toyota']
+# modals = ['figo', 'accord', 'prius']
+# years = [1960, 1970, 1980, 1990, 2000, 2010, 2022]
+# engine = 'gasoline'
+# features = []
+# cardict = {'brand':brands, 'modal':modals, 'year':years,'engine':engine, 'feature':features}
+# print(cardict)
+# print(cardict.keys())
+# print(type(cardict['feature']))
+# print(cardict.get('brand'))
+
+#  Sets
+
