cbff19894834cc718e375317a2fa514e48c5e6a2
Bhaveshpchavda <108624512+Bhaveshpchavda@users.noreply.github.com>
2023-05-03 17:44:04 +0530

    done

--- PYTHON/classW.py
+++ PYTHON/classW.py
@@ -245,4 +245,133 @@
 # plt.plot(range(1,11),values2,c='b',lw=2,ls=':',marker='o')
 # plt.show()
 
-import matplotlib.pyplot as plt 
+# class Person:
+#     name = "Bhavesh"
+#     Work = "Developer"
+#     def info(self):
+#         print(f"{self.name} is {self.work}")
+
+# a = Person()
+# a.name="Sahil"
+# a.work="man"
+# print(a.name , a.Work)
+# a.info()
+
+# class Person:
+#     def __init__(self ,n,w):
+#         self.name = n
+#         self.work = w 
+
+#     def info(self):
+#         print(f"{self.name} is {self.work}")
+
+# a = Person ("Bhavesh" , "Student")
+# b = Person("Sahil" , "ENgi")
+# a.info()
+# b.info()
+
+# class Employee:
+#     no_of_leaves = 8
+
+#     def __init__(self, aname, asalary, arole):
+#         self.name = aname
+#         self.salary = asalary
+#         self.role = arole
+
+#     def printdetails(self):
+#         return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+
+#     @classmethod
+#     def change_leaves(cls, newleaves):
+#         cls.no_of_leaves = newleaves
+
+#     def __add__(self, other):
+#         return self.salary + other.salary
+
+#     def __truediv__(self, other):
+#         return self.salary / other.salary
+
+#     def __repr__(self):
+#         return f"Employee('{self.name}', {self.salary}, '{self.role}')"
+
+#     def __str__(self):
+#         return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+
+# emp1 =Employee("Harry", 345, "Programmer")
+# emp2 =Employee("Rohan", 55, "Cleaner")
+# print(str(emp1))
+
+# import numpy as np 
+# myarr = np.array([1,33,5,7],np.int8)
+# print(myarr)
+
+import numpy as np
+import pandas as pd
+# from numpy import *
+# a= np.array(['ICT','Gnu','Ahmedabad'])
+# print(type(a))
+# print(a) 
+
+# ide = np.identity(45)
+# arr=np.arange(99)
+# arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print(arr)
+# print(type(np.where(arr>5)))
+# print(np.nonzero(arr))
+
+# b = np.arange(0,10,1) 
+# print(b)
+
+# c = np.zeros(5) 
+# print(c) 
+# c1 = np.zeros((5,5)) #have not to give as 
+# print(c1)
+# b = np.eye(5) 
+# print(b)
+# c = np.linspace(0,10,11) 
+# print(c)
+# re1 = np.random.randint(1,100,10) 
+# re2 = re1.reshape(5,2) 
+# print(re2)
+# s = pd.Series([1, 3, 5, 7, 9, 11]) 
+# print(s)
+# s = pd.Series([1, 3, 5, 7, 9, 11]) 
+# print("S[0] = ", s[0])
+# b = s[0] + s[1]
+# print("Sum = ", b)
+
+# from matplotlib import pyplot as plt 
+# plt.plot([1,2,3],[4,5,6])
+# plt.title("My Data")
+# plt.ylabel("Y")
+# plt.xlabel("X")
+# plt.show()
+# import matplotlib.pyplot as plt
+# values = [5,8,9,4,1,6,7,2,3,8]
+# plt.plot(range(1,11),values)
+# plt.show()
+
+# dic1={
+#     "name":["Bhavesh","Dev","Rohan","Shubh"],
+#     "marks":[89,91,67,86],
+#     "city":["Surat","Noida","Delhi","Panjab"]
+# }
+
+# df=pd.DataFrame(dic1)
+# print(df.describe)
+# df.to_csv("Ex.csv")
+
+
+# a={}
+# a[2]=10
+# a[1]=[22,33,44]
+# print(a[1][1])
+
+
+# def hello():
+#     try:
+#         return 1
+#     finally:
+#         return 2
+
+# print(hello())
