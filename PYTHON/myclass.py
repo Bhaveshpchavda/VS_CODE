fb9587cb240526956be94fa10e10fb6906541cf5
Bhaveshpchavda <108624512+Bhaveshpchavda@users.noreply.github.com>
2023-05-09 20:10:29 +0530

    initial commit

---
+++ PYTHON/myclass.py
@@ -0,0 +1,30 @@
+# print("Hello World")
+# print(5//2)
+
+# first = int(input("Enter First number : "))
+# opretor = input("Enter Opretor (+,-,/,*,%): ")
+# second = int(input("Enter Second Number: "))
+
+# if(opretor=="+"):
+#     print(first,opretor,second,"=",first+second)
+
+# if(opretor=="-"):
+#     print(first,opretor,second,"=",first-second)
+
+# if(opretor=="/"):
+#     print(first,opretor,second,"=",first/second)
+
+# if(opretor=="*"):
+#     print(first,opretor,second,"=",first*second)
+
+# if(opretor=="%"):
+#     print(first,opretor,second,"=",first%second)
+
+# for i in range(10000):
+#     print(i)
+
+# i=1
+# while i<5:
+#     print(i)
+#     i += 1
+
