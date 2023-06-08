f09427931300bd5c5b0cb374a57de34646f95a73
bhaveshpchavda <bhaveshpchavda21@gnu.ac.in>
2023-06-08 16:24:48 +0530

    initial commit

---
+++ REACT_Js/minor/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    
+    <div className="container">From Tailwind</div>
+
+  );
+  }
+
+export default App;
